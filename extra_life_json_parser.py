4f440b99bad964ea7d403570c58dceb8da29861d
Spencer Watza <s.g.watza@gmail.com>
2018-11-03 17:19:18 -0600

    added step through with keyboard input

--- extra_life_json_parser.py
+++ extra_life_json_parser.py
@@ -3,6 +3,7 @@ import json
 #Load the json file
 filename = 'json_donation_file.txt'
 i = 0
+flag100_set = 0
 with open(filename,'r') as f:
     all_data = json.load(f)
     while i < len(all_data):
@@ -13,10 +14,17 @@ with open(filename,'r') as f:
             output_string = 'Donation ' + str(data['amount']) + ' by ' + str(data['displayName']) + ': '
             if str(data['message']) != "None":
                 output_string += str(data['message'])
-            if(data['amount'] >= 100.0):
+            if data['amount'] >= 100.0 and flag100_set:
                 print(output_string + '\n')
+            elif not flag100_set:
+                print(output_string + '\n')
+
         except ValueError as err:
             print("Error parsing String! Sorry I'm lazy\n")
             #print(err)
             #print('\n')
+        try:
+            useless = input("Press Enter for next 100$+ donation\n")
+        except SyntaxError:
+            pass
         i+= 1
